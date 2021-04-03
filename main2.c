b3e04fd2793ab77a3171e4e19dd104c978068917
Aaronk7 <67217076+Aaronk7@users.noreply.github.com>
2021-04-03 15:11:34 -0700

    Update main2.c
    added basic switch cases

--- main2.c
+++ main2.c
@@ -345,9 +345,37 @@ void call_I_format(int* code, char** reg_arr, int opcode, int* sign_extended){
 
 void decode(char* ins, int* sign_extended){
     int opcode = 0;
-    int* machineCode = (int*) malloc(32*sizeof(int));
-    
+    int* machineCode = (int*) malloc(32*sizeof(int));   
     printf("Machine code ins: %s\n", ins);
+    
+    
+    
+    /* switch(operation) {     // need to assign operation
+        case "add":
+            add = "0010";
+        case "subtract":
+            sub = "0110";
+        case "or":
+            or = "0001";
+        case "and":
+            and = "0000";
+        case "slt":
+            slt = "0111";
+        case "nor":
+            nor = "1100";
+        case "beq":
+            beq = "0110";   // subtract
+        case "lw":
+            nor = "lw";   // add
+        case "sw":
+            sw = "0010";  // add
+            
+    }
+    
+    */
+    
+    
+    
     
     //Transfer User input char string into array of integers machineCode
     for(int i = 0;i < 32; i++){
