a66f92da500115a773b4cefa3523e803f5c05f5f
Aaronk7 <67217076+Aaronk7@users.noreply.github.com>
2021-04-02 23:22:34 -0700

    Add files via upload

--- main2.c
+++ main2.c
@@ -15,6 +15,8 @@ char **char_registers = (char *[]) {"zero", "at", "v0", "v1", "a0", "a1", "a2", 
 
 
 char **fetch(int pc) {
+    
+    //FILE *fsize = fopen("sample_binary.txt","r");
     char ch;
     int count = 100;
     int local_pc = pc;
@@ -44,50 +46,6 @@ char **fetch(int pc) {
     return ins_memory;
 }
 
-
-
-void decode(char* ins, int* sign_extended){
-    int opcode = 0;
-	int* machineCode = (int*) malloc(32*sizeof(int));
-	
-    printf("Machine code ins: %s\n", ins);
-	
-	//Transfer User input char string into array of integers machineCode
-	for(int i = 0;i < 32; i++){
-		//since ASCII code for '0' is 48 and '1' is 49, use -48 to get the correct integer
-		*(machineCode+i) = *(ins+i) - 48;
-	}
-	
-    for(int i = 0;i <= 5; i++){ //opcode 0-5 has 6 bits
-        opcode = opcode + (*(machineCode+i)*twoToPower(5-i));//element 0-5 correspond to 2^5...2^0
-    }
-    
-    //printf("Opcode: %d \n",opcode);
-    
-    //0->R format
-    //2 or 3 ->J format
-    //Otherwise, 1,4-62->I format
-    
-    if(opcode == 0){
-        call_R_format(machineCode, char_registers);
-    }
-    else if(opcode == 2 || opcode == 3){
-        call_J_format(machineCode, opcode);
-    }
-    else{
-        call_I_format(machineCode, char_registers, opcode, sign_extended);
-    }
-}
-
-void printArr(int *a, int size){
-	//printf("printArr: "); 
-	for(int i = 0 ; i < size;i++){
-		printf("%d",*(a+i));
-	}
-	printf("\n");
-}
-
-
 int twoToPower(int power){//implement a for loop for powering
     int result = 1;
     for(int i = 1;i <= power; i++){
@@ -95,7 +53,34 @@ int twoToPower(int power){//implement a for loop for powering
     }
     return result;
 }
+void printArr(int *a, int size){
+    //printf("printArr: ");
+    for(int i = 0 ; i < size;i++){
+        printf("%d",*(a+i));
+    }
+    printf("\n");
+}
 
+//Not zero extension, sign_extension extend sign base on left most bit here
+void sign_extension(int sign_bit, int* code, int* sign_extended){
+    for(int i = 16;i <= 31; i++){ //imm 16-31 has 16 bits
+        *(sign_extended+i) = *(code+i);
+    }
+    if(sign_bit == 1){
+        for(int i = 0;i <= 15; i++){ //imm 16-31 has 16 bits
+            *(sign_extended+i) = 1;
+        }
+        printf("Sign extended code:");
+        printArr(sign_extended, 32);
+    }
+    else{
+        for(int i = 0;i <= 15; i++){ //imm 16-31 has 16 bits
+            *(sign_extended+i) = 0;
+        }
+        printf("Sign extended code:");
+        printArr(sign_extended, 32);
+    }
+}
 void call_R_format(int* code, char** reg_arr){
     printf("Instruction Type: R \n");
     
@@ -239,7 +224,7 @@ void call_I_format(int* code, char** reg_arr, int opcode, int* sign_extended){
        }
        
     printf("Operation: %s \n",i_operation);
-	
+    
     
     for(int i = 6;i <= 10; i++){ //rs 6-10 has 5 bits
         rs = rs + (*(code+i)*twoToPower(4-counter_power));//TwoToPower(4-counter_power) gets 2^4...2^0
@@ -249,146 +234,100 @@ void call_I_format(int* code, char** reg_arr, int opcode, int* sign_extended){
     printf("Rs: %s", reg_arr[rs]);
     printf("(R%d)\n",rs);
     
+    
     counter_power = 0;
     for(int i = 11;i <= 15; i++){ //rt 11-15 has 5 bits
         rt = rt + (*(code+i)*twoToPower(4-counter_power));//TwoToPower(4-counter_power) gets 2^4...2^0
         counter_power++;
     }
-	
     
     printf("Rt: %s", reg_arr[rt]);
     printf("(R%d)\n",rs);
-	
-	
-    //assume if immediate offet is positive
-	counter_power = 0;
-	for(int i = 16;i <= 31; i++){ //imm 16-31 has 16 bits
-		immediate = immediate + (*(code+i)*twoToPower(15-counter_power));//TwoToPower(15-counter_power) gets 2^15...2^0
-		counter_power++;
-	}
-    //
-	
-	//strcmp(...,...) == 0 means the two string are the same
-	if(strcmp(i_operation,"lw") == 0 || strcmp(i_operation,"sw") == 0){
-		
-		printf("leftmost bit: %d\n", *(code+16));
-		
-		//16 is the leftmost bit of the immediate
-		if(*(code+16) == 1){
-			printf("Negative offset\n");
-			sign_extension(*(code+16),code, sign_extended);
-			
-			//convertNegBinaryToDecimal using 2's complement
-			immediate = convertNegBinaryToDecimal(code);
-			
-			printf("Immediate: %d\n",immediate);
-		}
-		else{
-			printf("Positive offset\n");
-			sign_extension(*(code+16),code, sign_extended);
-			printf("Immediate: %d\n",immediate);
-		}
-		
-	}
-	else{
-		if(*(code+16) == 1){
-			printf("Negative offset\n");
-			
-			//convertNegBinaryToDecimal using 2's complement
-			immediate = convertNegBinaryToDecimal(code);
-			
-			printf("Immediate: %d\n",immediate);
-		}
-		else{
-			printf("Positive offset\n");
-			printf("Immediate: %d\n",immediate);
-		}
-		
-	}
-	
+    
+    counter_power = 0;
+    for(int i = 16;i <= 31; i++){ //imm 16-31 has 16 bits
+        immediate = immediate + (*(code+i)*twoToPower(15-counter_power));//TwoToPower(15-counter_power) gets 2^15...2^0
+        counter_power++;
+    }
+    
+    //strcmp(...,...) == 0 means the two string are the same
+    if(strcmp(i_operation,"lw") == 0 || strcmp(i_operation,"sw") == 0){
+        
+        printf("leftmost bit: %d\n", *(code+16));
+        
+        //16 is the leftmost bit of the immediate
+        if(*(code+16) == 1){
+            printf("Negative offset\n");
+            sign_extension(*(code+16),code, sign_extended);
+            printf("Immediate: -%d\n",immediate);
+        }
+        else{
+            printf("Positive offset\n");
+            sign_extension(*(code+16),code, sign_extended);
+            printf("Immediate: %d\n",immediate);
+        }
+    }
+    
+    //printf("Immediate: %d\n",immediate);
     
    }
 
 
-//Not zero extension, sign_extension extend sign base on left most bit here
-void sign_extension(int sign_bit, int* code, int* sign_extended){
-	for(int i = 16;i <= 31; i++){ //imm 16-31 has 16 bits
-        *(sign_extended+i) = *(code+i);
+
+
+
+void decode(char* ins, int* sign_extended){
+    int opcode = 0;
+    int* machineCode = (int*) malloc(32*sizeof(int));
+    
+    printf("Machine code ins: %s\n", ins);
+    
+    //Transfer User input char string into array of integers machineCode
+    for(int i = 0;i < 32; i++){
+        //since ASCII code for '0' is 48 and '1' is 49, use -48 to get the correct integer
+        *(machineCode+i) = *(ins+i) - 48;
     }
-	if(sign_bit == 1){
-		for(int i = 0;i <= 15; i++){ //imm 16-31 has 16 bits
-			*(sign_extended+i) = 1;
-		}
-		printf("Sign extended code:");
-		printArr(sign_extended, 32);
-	}
-	else{
-		for(int i = 0;i <= 15; i++){ //imm 16-31 has 16 bits
-			*(sign_extended+i) = 0;
-		}
-		printf("Sign extended code:");
-		printArr(sign_extended, 32);
-	}
+    
+    for(int i = 0;i <= 5; i++){ //opcode 0-5 has 6 bits
+        opcode = opcode + (*(machineCode+i)*twoToPower(5-i));//element 0-5 correspond to 2^5...2^0
+    }
+    
+    //printf("Opcode: %d \n",opcode);
+    
+    //0->R format
+    //2 or 3 ->J format
+    //Otherwise, 1,4-62->I format
+    
+    if(opcode == 0){
+        call_R_format(machineCode, char_registers);
+    }
+    else if(opcode == 2 || opcode == 3){
+        call_J_format(machineCode, opcode);
+    }
+    else{
+        call_I_format(machineCode, char_registers, opcode, sign_extended);
+    }
 }
-
-
-int convertNegBinaryToDecimal(int* code){
-	int immediate = 0, counter_power = 0;
-	
-	//use 2's complement to convert imm binary to negative decimal
-	//Flip the 16 bits for imm and store it in flip_imm
-	int* flip_imm = (int*)malloc(16*sizeof(int));
-	for(int i = 16;i <= 31; i++){ //imm 16-31 has 16 bits
-		if(*(code+i) == 1){
-			//-16 to start at index 0 in flip_imm
-			*(flip_imm + i - 16) = 0;
-		}
-		else{
-			*(flip_imm + i - 16) = 1;
-		}
-				
-	}
-			
-	printf("After Flip 16 bits:");
-	printArr(flip_imm, 16);
-			
-	counter_power = 0;
-			
-	for(int i = 0;i <= 15; i++){ //imm 16-31 has 16 bits
-		immediate = immediate + (*(flip_imm+i)*twoToPower(15-counter_power));//TwoToPower(15-counter_power) gets 2^15...2^0
-		counter_power++;
-	}
-			
-	//add 1 to the flip bits for 2's complement
-	immediate += 1;
-			
-	//make the immediate negative
-	immediate *= -1;	
-	
-	return immediate;
-}	
-
 
 
 
 int main(){
-	//value that store inside the Registerfile
-	int* registerfile = (int*) malloc(32*sizeof(int));
+    //value that store inside the Registerfile
+    int* registerfile = (int*) malloc(32*sizeof(int));
 
-	//array to store sign-extended offet
-	int* sign_extended = (int*) malloc(32*sizeof(int));
-	
+    //array to store sign-extended offet
+    int* sign_extended = (int*) malloc(32*sizeof(int));
+    
     ins_memory = fetch(pc);
     
     for(int i = 0; i < 8; i++){
         decode(*(ins_memory + i), sign_extended);
-		/* Debug for what each instruction array stores decimal 48 as char 1
-		for(int j = 0; j < 32; j++){
-			printf("%s\n", *(*(ins_memory+i)+j));
-		}
-		printf("next");
-		*/
-		printf("\n");
+        /* Debug for what each instruction array stores decimal 48 as char 1
+        for(int j = 0; j < 32; j++){
+            printf("%s\n", *(*(ins_memory+i)+j));
+        }
+        printf("next");
+        */
     }
     
     return 0;
